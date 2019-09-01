cfca6ace27c408270b6f2ea95643d81402a8a245
Alexande B <abobrikovich@gmail.com>
2019-09-02 00:52:36 +0300

    Add first version of release-tool

---
+++ release-tool/lib/metanorma/release/tool.rb
@@ -0,0 +1,42 @@
+require "rubygems/commands/dependency_command"
+
+module Metanorma
+  module Release
+    module Tool
+      class Error < StandardError; end
+
+      def self.gems(path)
+      	gitmodules_path = File.join(path, '.gitmodules')
+      	@gems ||= File.open(File.join(path, '.gitmodules'), 'r:UTF-8').each_line.map { |line|
+      		line.match(/path\s*=\s*([\w_-]+)$/)
+      	}.compact.map { |m| m[1]}
+      rescue Errno::ENOENT => e
+      	raise "#{gitmodules_path} not found make sure that -p/--gems-path correctly specified"
+      end
+
+      def self.gem_deps(path)
+      	gems = self.gems(path)
+      	@gem_deps ||= gems.map { |g|
+      		begin
+      			gemspec_path = File.join(path, g, "#{g}.gemspec")
+	      		deps = File.open(File.join(path, g, "#{g}.gemspec"), 'r:UTF-8').each_line.map { |line|
+	      		 	line.match(/add_(development_|runtime_)?dependency\s+.([\w_-]+)/)
+	      		}.compact.map { |m| m[2]}.keep_if { |d| gems.include?(d) }.uniq
+      		rescue Errno::ENOENT => e
+		      	puts "[!] gemspec #{gemspec_path} not found for #{g} gem"
+		      end
+
+      		[g, deps]
+      	}.to_h
+      end
+
+      def self.gem_tests(path)
+      	# https://stackoverflow.com/q/28655221/902217
+      	@gem_tests ||= self.gem_deps(path).inject(Hash.new([])) { |memo, (key, values)|
+				  values.each { |value| memo[value] += [key] }
+				  memo
+				}
+      end
+    end
+  end
+end
