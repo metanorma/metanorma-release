70775f9b3bd90eedd5506f52a21ff6be084ac18a
Ronald Tse <ronald.tse@ribose.com>
2019-09-12 16:59:45 +0800

    Remove release-tool

--- release-tool/lib/metanorma/release/tool/version.rb
+++
@@ -1,7 +0,0 @@
-module Metanorma
-  module Release
-    module Tool
-      VERSION = "0.1.0"
-    end
-  end
-end
--- release-tool/spec/metanorma/release/tool_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Metanorma::Release::Tool do
-  it "has a version number" do
-    expect(Metanorma::Release::Tool::VERSION).not_to be nil
-  end
-
-  it "gems not empty for valid path" do
-    expect(Metanorma::Release::Tool::gems('../')).to match_array []
-  end
-end
